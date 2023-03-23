d9782e885347806c75014c9a4bfdf110ea04fa5a
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2023-03-23 11:33:10 +0900

    :sparkle: add experiment adata

--- ctl.py
+++ ctl.py
@@ -5,7 +5,7 @@ import numpy as np
 aruco = cv2.aruco
 
 BACKGROUND_LOOPCNT = 5
-SERIAL_PORT = "COM3"
+SERIAL_PORT = "COM6"
 
 
 class PillowDevice(object):
@@ -41,11 +41,12 @@ class PillowDevice(object):
         self.serial.write(command.encode())
     
     def lift_up(self):
-        command = "w 15000"
+        command = "w 12000"
+        print(command)
         self.serial.write(command.encode())
 
     def lift_down(self):
-        command = "s 15000"
+        command = "s 12000"
         self.serial.write(command.encode())
 
 
@@ -61,10 +62,10 @@ def get_marker_center(corners, ids):
 
 
 def main():
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     background_frame = None
     fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
-    dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    # dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
     device = PillowDevice(port = SERIAL_PORT)
     move_flag = False
     projection_ratio_buff = np.array([0.5 for i in range(5)])
@@ -82,88 +83,94 @@ def main():
             device.lift_up()
         elif key == 93: #down
             device.lift_down()
+        elif key == 97: # A
+            device.move("left")
+        elif key == 115: # S
+            device.move("center")
+        elif key == 100: # D
+            device.move("right")
         
-        #背景の適用
-        if background_frame is not None:
-            for _ in range(BACKGROUND_LOOPCNT):
-                fgbg.apply(background_frame)
-        subtracted_frame = fgbg.apply(frame)
+        # #背景の適用
+        # if background_frame is not None:
+        #     for _ in range(BACKGROUND_LOOPCNT):
+        #         fgbg.apply(background_frame)
+        # subtracted_frame = fgbg.apply(frame)
 
-        #マーカー検出
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(frame,dictionary)
-        center_dict = get_marker_center(corners,ids)
+        # #マーカー検出
+        # corners, ids, rejectedImgPoints = aruco.detectMarkers(frame,dictionary)
+        # center_dict = get_marker_center(corners,ids)
 
-        if 0 in center_dict.keys() and 1 in center_dict.keys():
-            line_frame = np.zeros_like(subtracted_frame)
+        # if 0 in center_dict.keys() and 1 in center_dict.keys():
+        #     line_frame = np.zeros_like(subtracted_frame)
 
-            #線の描画
-            cv2.line(
-                line_frame,
-                tuple(center_dict[0].astype(np.int32)),
-                tuple(center_dict[1].astype(np.int32)),
-                255,
-                thickness = 20,
-            )
+        #     #線の描画
+        #     cv2.line(
+        #         line_frame,
+        #         tuple(center_dict[0].astype(np.int32)),
+        #         tuple(center_dict[1].astype(np.int32)),
+        #         255,
+        #         thickness = 20,
+        #     )
 
-            contour_frame = np.zeros_like(subtracted_frame)
-            contours, hierarchy = cv2.findContours(
-                subtracted_frame,
-                cv2.RETR_EXTERNAL, 
-                cv2.CHAIN_APPROX_SIMPLE
-            )
-            cv2.drawContours(
-                contour_frame,
-                contours,
-                -1,
-                255,
-                -1
-            )
+        #     contour_frame = np.zeros_like(subtracted_frame)
+        #     contours, hierarchy = cv2.findContours(
+        #         subtracted_frame,
+        #         cv2.RETR_EXTERNAL, 
+        #         cv2.CHAIN_APPROX_SIMPLE
+        #     )
+        #     cv2.drawContours(
+        #         contour_frame,
+        #         contours,
+        #         -1,
+        #         255,
+        #         -1
+        #     )
 
-            target_frame = np.where((contour_frame == 255) & (line_frame == 255), 255, 0)
-            contours, hierarchy = cv2.findContours(
-                target_frame.astype(np.uint8),
-                cv2.RETR_EXTERNAL, 
-                cv2.CHAIN_APPROX_SIMPLE
-            )
-            contours = sorted(contours,key = lambda contour: -cv2.contourArea(contour))
-            output_area = np.zeros_like(subtracted_frame)
-            cv2.drawContours(
-                output_area,
-                contours[:2],
-                -1,
-                255,
-                -1
-            )
-            cv2.imshow("output_area",output_area.astype(np.uint8))
+        #     target_frame = np.where((contour_frame == 255) & (line_frame == 255), 255, 0)
+        #     contours, hierarchy = cv2.findContours(
+        #         target_frame.astype(np.uint8),
+        #         cv2.RETR_EXTERNAL, 
+        #         cv2.CHAIN_APPROX_SIMPLE
+        #     )
+        #     contours = sorted(contours,key = lambda contour: -cv2.contourArea(contour))
+        #     output_area = np.zeros_like(subtracted_frame)
+        #     cv2.drawContours(
+        #         output_area,
+        #         contours[:2],
+        #         -1,
+        #         255,
+        #         -1
+        #     )
+        #     cv2.imshow("output_area",output_area.astype(np.uint8))
 
-            if np.any(target_frame == 255):
-                #重心の算出
-                M = cv2.moments(output_area.astype(np.uint8))
-                if M["m00"] != 0:
-                    x,y= int(M["m10"]/M["m00"]) , int(M["m01"]/M["m00"])
-                    cv2.circle(frame, (x,y), 10, 255, -1)
+        #     if np.any(target_frame == 255):
+        #         #重心の算出
+        #         M = cv2.moments(output_area.astype(np.uint8))
+        #         if M["m00"] != 0:
+        #             x,y= int(M["m10"]/M["m00"]) , int(M["m01"]/M["m00"])
+        #             cv2.circle(frame, (x,y), 10, 255, -1)
                     
-                    #枕に対する位置を算出
-                    zero2one_vector = center_dict[1]-center_dict[0]
-                    zero2one_vector_norm = zero2one_vector / np.linalg.norm(zero2one_vector)
-                    zero2target_vector = np.array([x,y])-center_dict[0]
-                    zero2target_projection = np.dot(zero2one_vector_norm,zero2target_vector)*zero2one_vector_norm
-                    projection_ratio = np.linalg.norm(zero2target_projection) / np.linalg.norm(zero2one_vector)
-                    projection_ratio_buff = np.append(projection_ratio_buff,projection_ratio)[1:] 
+        #             #枕に対する位置を算出
+        #             zero2one_vector = center_dict[1]-center_dict[0]
+        #             zero2one_vector_norm = zero2one_vector / np.linalg.norm(zero2one_vector)
+        #             zero2target_vector = np.array([x,y])-center_dict[0]
+        #             zero2target_projection = np.dot(zero2one_vector_norm,zero2target_vector)*zero2one_vector_norm
+        #             projection_ratio = np.linalg.norm(zero2target_projection) / np.linalg.norm(zero2one_vector)
+        #             projection_ratio_buff = np.append(projection_ratio_buff,projection_ratio)[1:] 
 
-                    position = "uncear"
-                    if np.all(projection_ratio_buff >= 0.67):
-                        position = "left"
-                    elif np.all(projection_ratio_buff <= 0.32):
-                        position = "right"
-                    elif np.all(projection_ratio_buff > 0.32) and np.all(projection_ratio_buff < 0.67):
-                        position = "center"
+        #             position = "uncear"
+        #             if np.all(projection_ratio_buff >= 0.67):
+        #                 position = "left"
+        #             elif np.all(projection_ratio_buff <= 0.32):
+        #                 position = "right"
+        #             elif np.all(projection_ratio_buff > 0.32) and np.all(projection_ratio_buff < 0.67):
+        #                 position = "center"
                     
 
-                    if move_flag:
-                        if position != "unclear":
-                            move_flag = False
-                            device.move(position)
+        #             if move_flag:
+        #                 if position != "unclear":
+        #                     move_flag = False
+        #                     device.move(position)
 
             
         
